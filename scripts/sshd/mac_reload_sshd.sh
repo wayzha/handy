2827b5ad4701f52b1bf29ed831d0d7cdd1d5c42b
i047794 <wayne.zhang@sap.com>
2021-12-09 13:44:24 +0800

    update

--- scripts/sshd/mac_reload_sshd.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-sudo launchctl stop com.openssh.sshd
-sudo launchctl start com.openssh.sshd
-
-sudo launchctl unload /System/Library/LaunchDaemons/ssh.plist
-sudo launchctl load -w /System/Library/LaunchDaemons/ssh.plist
