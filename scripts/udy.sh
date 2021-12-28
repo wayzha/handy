dbf944ac54e966ec62999692c6e24115d7133dbb
Wayne Zhang <wayne.zhang@sap.com>
2021-12-28 13:27:56 +0800

    update

--- scripts/udy.sh
+++ scripts/udy.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+SERVER_URL="www.google.com"
+
+wget -q -t 1 --timeout=1 --spider $SERVER_URL
+if [ $? != 0 ]; then
+  if [[ $(command -v proxy-localhost.sh) != "" ]]; then
+    echo "-----> Set Proxy."
+    source proxy-localhost.sh
+    wget -q -t 1 --timeout=1 --spider $SERVER_URL
+    if [ $? != 0 ]; then
+      echo "-----> Unset Proxy."
+      source proxy-unset.sh
+    fi
+  fi
+fi
+
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   # Linux
   if [[ $(command -v apt) != "" ]];  then
@@ -19,7 +34,7 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
     fi
 else
     # Unknown OS
-    echo "Unknown OS. No Package Update!"
+    echo "-----> Unknown OS. No Package Update!"
 fi
 
 if [[ -d ~/workspace/git/handy ]]; then
