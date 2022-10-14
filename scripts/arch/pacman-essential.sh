19ba97964228157dabaa9dcbbe49d3dafb8bdaaa
Wayne Zhang <way.zha@gmail.com>
2022-10-14 17:41:23 +0800

    update

--- scripts/arch/pacman-essential.sh
+++ scripts/arch/pacman-essential.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 sudo pacman -S lsof reflector tmux traceroute neofetch htop autojump man-db vim zsh base-devel yay dnsutils wget --noconfirm
+sudo pacman -S net-tools dnsutils inetutils iproute2 --noconfirm
