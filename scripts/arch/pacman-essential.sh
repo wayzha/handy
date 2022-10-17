c59c58e42cdcebe2c64fed756e74361c22b9070b
i047794 <wayne.zhang@sap.com>
2022-10-17 21:27:10 +0800

    update

--- scripts/arch/pacman-essential.sh
+++ scripts/arch/pacman-essential.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
-sudo pacman -S lsof reflector tmux traceroute neofetch htop autojump man-db vim zsh base-devel yay dnsutils wget --noconfirm
-sudo pacman -S net-tools dnsutils inetutils iproute2 --noconfirm
+sudo pacman -S reflector --noconfirm
+
+sudo pacman -S vim git zsh -noconfirm
+sudo pacman -S base-devel --noconfirm
+sudo pacman -S lsof tmux autojump neofetch htop man-db --noconfirm
+sudo pacman -S traceroute wget net-tools dnsutils inetutils iproute2 --noconfirm
+
+sudo pacman -S yay --noconfirm
