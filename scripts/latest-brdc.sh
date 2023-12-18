585945479de2447fd7c04d52e0b04391dce5919b
Wayne Zhang <way.zha@gmail.com>
2023-12-18 20:42:16 +0800

    update

--- scripts/latest-brdc.sh
+++ scripts/latest-brdc.sh
@@ -5,7 +5,8 @@ set -eu
 current_year=$(date +%Y)
 
 ## get latest version
-latest_version=$(wget -qO- "ftp://gssc.esa.int/gnss/data/daily/$current_year/brdc/" | awk -F'/brdc*/' '/brdc*/{print $2}' | cut -d\" -f1 | grep -v '\index.html' | tail -1)
+# latest_version=$(wget -qO- "ftp://gssc.esa.int/gnss/data/daily/$current_year/brdc/" | awk -F'/brdc*/' '/brdc*/{print $2}' | cut -d\" -f1 | grep -v '\index.html' | tail -1)
+latest_version=$(wget -qO- "ftp://gssc.esa.int/gnss/data/daily/2023/brdc/" | awk -F "/brdc/" '{print $2}' | cut -d \" -f1 | grep 'n.gz' | tail -1)
 
 ## download
 wget "ftp://gssc.esa.int/gnss/data/daily/$current_year/brdc/$latest_version"
